ec0ebaa219f2e3be4290e2e004588176ddf5ec68
Pandarang <whalsguddlek@gmail.com>
2026-09-17 14:48:29 +0900

    0007

--- src/main/java/com/back/global/initData/DataInit.java
+++ src/main/java/com/back/global/initData/DataInit.java
@@ -1,7 +1,7 @@
 package com.back.global.initData;
 
-import com.back.boundedContext.member.entity.Member;
-import com.back.boundedContext.member.service.MemberService;
+import com.back.boundedContext.member.domain.Member;
+import com.back.boundedContext.member.app.MemberService;
 import com.back.boundedContext.post.entity.Post;
 import com.back.boundedContext.post.service.PostService;
 import lombok.extern.slf4j.Slf4j;
